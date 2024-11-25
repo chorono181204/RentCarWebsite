91dd7e93100cf0e227bb0788e28443c58f235648
albert2704 <albert04.dev@gmail.com>
2024-11-25 16:58:04 +0700

    modify to pull

--- src/java/util/JDBCConnect.java
+++ src/java/util/JDBCConnect.java
@@ -12,18 +12,19 @@ public class JDBCConnect {
     static final String USER = "root";
     static final String PASS = "123456";
 
-	public static Connection getConnection() {
-   
-		Connection conn = null;
-		try {   
-                        Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		} catch (SQLException e) {
-			e.printStackTrace();
-                        System.out.println("Connect Database Failed");
-		} catch (ClassNotFoundException ex) {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {   
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connect Database Failed");
+        } 
+        catch (ClassNotFoundException ex) {
             Logger.getLogger(JDBCConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
-		return conn;
-	}
+        return conn;
+    }
 }
